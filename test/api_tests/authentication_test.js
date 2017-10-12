5f75b361d1a5b4a7b5a5198ad29cf1a58f3a1789
Matthew Barnden <matt@aheadmedia.co.uk>
2017-10-12 08:07:51 +0100

    fixed supertest to a point - still needs nesting describe

--- test/api_tests/authentication_test.js
+++ test/api_tests/authentication_test.js
@@ -1,20 +1,20 @@
 import {expect} from 'chai';
 import SuperTest from 'supertest';
 import Should from 'should';
+import express from 'express';
 
-const server = SuperTest.agent("http://localhost:75");
+const app = require('../../lib/server');
+
 
 let token,
     userId;
+    
+  describe('Api::when a user registers', () => {
 
-describe('Authentication', () => {
-
-  describe('Api::when a user registers, it', () => {
-
-    it('should return the user a token when user supplies name, password and email', (done) => {
-        server
+    it('it should return the user a token when user supplies name, password and email', (done) => {
+        SuperTest(app)
         .post('/api/users')
-        .send({name : 'Super Test', password : 'Password', email : 'email@supertester.com'})
+        .send({'name' : 'Super Test', password : 'Password', email : 'email@supertester.com'})
         .expect("Content-type",/json/)
         .expect(200)
         .end(function(err,res){
@@ -27,21 +27,20 @@ describe('Authentication', () => {
     });
 
     it('should allow the user to access data requiring validation when requesting with token', (done) => {
-        server
+      SuperTest(app)
         .get('/api/users')
         .send({userId: userId, token:token})
         .expect("Content-type",/json/)
         .expect(200)
         .end(function(err,res){
           res.status.should.equal(200);
-          var length = res.body.length;
-          res.body[res.body.length -1].name.should.equal('Super Test');
+          res.body.users[res.body.users.length -1].name.should.equal('Super Test');
           done();
         });
     });
 
-    it('should not allow another user to use the newly created users email', (done) => {
-        server
+    it('it should not allow another user to use the newly created users email', (done) => {
+      SuperTest(app)
         .post('/api/users')
         .send({name : 'Super Test2', password : 'Password2', email : 'email@supertester.com'})
         .expect("Content-type",/json/)
@@ -56,8 +55,8 @@ describe('Authentication', () => {
 
   describe('Api::when a users token has expired and he needs to re-authenticate, it', () => {
 
-    it('should allow the user to re-authenticate with their email and password', (done) => {
-        server
+    it('it should allow the user to re-authenticate with their email and password', (done) => {
+      SuperTest(app)
         .post('/api/authenticate')
         .send({email : 'email@supertester.com', password : 'Password'})
         .expect("Content-type",/json/)
@@ -71,22 +70,21 @@ describe('Authentication', () => {
         });
     });
 
-    it('should allow the user to login to pages requiring validation when requesting with the new token', (done) => {
-        server
+    it('it should allow the user to login to pages requiring validation when requesting with the new token', (done) => {
+      SuperTest(app)
         .get('/api/users')
         .send({userId:userId, token:token})
         .expect("Content-type",/json/)
         .expect(200)
         .end(function(err,res){
           res.status.should.equal(200);
-          var length = res.body.length;
-          res.body[res.body.length -1].name.should.equal('Super Test');
+          res.body.users[res.body.users.length -1].name.should.equal('Super Test');
           done();
         });
     });
 
-    it('should not allow the user to re-authenticate with incorrect password', (done) => {
-        server
+    it('it should not allow the user to re-authenticate with incorrect password', (done) => {
+      SuperTest(app)
         .post('/api/authenticate')
         .send({email : 'email@supertester.com', password : 'password-wrong'})
         .expect("Content-type",/json/)
@@ -102,8 +100,8 @@ describe('Authentication', () => {
 
   describe('Api::when a user tries to authenticate and does not provide full credentials', () => {
 
-    it('should not supply a token', (done) => {
-        server
+    it('it should not supply a token', (done) => {
+      SuperTest(app)
         .post('/api/users')
         .send({email : 'email@supertester.com', password : 'Password'})
         .expect("Content-type",/json/)
@@ -119,8 +117,8 @@ describe('Authentication', () => {
 
   describe('Api::when a user tries to access data that requires authentication without a token', () => {
 
-    it('should not allow access to the data', (done) => {
-        server
+    it('it should not allow access to the data', (done) => {
+      SuperTest(app)
         .get('/api/users')
         .send({userId:userId, token:''})
         .expect("Content-type",/json/)
@@ -136,8 +134,8 @@ describe('Authentication', () => {
 
   describe('Api::when a user deletes himself', () => {
 
-    it('should return success message including the users deleted id', (done) => {
-        server
+    it('it should return success message including the users deleted id', (done) => {
+      SuperTest(app)
         .delete(`/api/users/${userId}`)
         .send({token:token, userId:userId})
         .expect("Content-type",/json/)
@@ -149,8 +147,8 @@ describe('Authentication', () => {
         });
     });
 
-    it('should not allow the user to authenticate with deleted name and password', (done) => {
-        server
+    it('it should not allow the user to authenticate with deleted name and password', (done) => {
+      SuperTest(app)
         .post('/api/authenticate')
         .send({email : 'email@supertester.com', password : 'Password'})
         .expect("Content-type",/json/)
@@ -161,7 +159,5 @@ describe('Authentication', () => {
           done();
         });
     });
-
   });
   
-});
